c871162b8a899939d3d55158554b22bf78f1b7d9
vaishnav vernekar <vernekarvaishnav05@gmail.com>
2025-07-18 16:43:03 +0530

    t

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
+  
 })
 
 
