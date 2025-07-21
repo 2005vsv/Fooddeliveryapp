f6e50fb55ee6760d64c9511573e37411a7c5eb93
vaishnav vernekar <vernekarvaishnav05@gmail.com>
2025-07-21 17:28:16 +0530

    m

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -33,7 +33,13 @@ exports.login = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user || user.password !== password) {
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    // Use bcrypt to compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).json({ error: "Invalid credentials" });
     }
 
